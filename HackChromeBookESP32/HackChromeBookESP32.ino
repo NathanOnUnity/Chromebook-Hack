8a4cdbede79cb2d5cd3c72b21a75266fffae28c2
NathanOnUnity <154831290+NathanOnUnity@users.noreply.github.com>
2024-10-06 22:04:41 -0400

    Working on Async
    I want connection indication

--- HackChromeBookESP32/HackChromeBookESP32.ino
+++ HackChromeBookESP32/HackChromeBookESP32.ino
@@ -4,16 +4,61 @@
 #include <BleKeyboard.h>
 
 BleKeyboard bleKeyboard;
+unsigned long previousMillis = 0;
+unsigned long currentMillis = 0;
+
+bool lastDelayDone = false;
+bool canHack = false;
+bool delayDone[7] = {false,false,false,false,false,false,false};
+int delayItteration = 0;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting BLE");
   bleKeyboard.begin();
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
+  currentMillis = millis();
+
   if(bleKeyboard.isConnected()) {
-    Serial.println("New Window");
+    if(canHack == true)
+    {
+      HackTheChromeBookV2();
+    }
+    digitalWrite(2, HIGH);
+  }else
+  {
+    digitalWrite(2, LOW);
+    Serial.println("Disconnected");
+  }
+
+ // Serial.println("Waiting 5 seconds...");
+ // delay(5000);
+}
+void AsyncDelay(float delayAmount){
+
+  if(currentMillis - previousMillis >= delayAmount){
+
+    lastDelayDone = true;
+    previousMillis = currentMillis;
+    delayItteration++;
+    delayDone[delayItteration-1] = true;
+  }else{
+
+    lastDelayDone = false;
+  }
+}
+
+void HackDelay(){
+
+  AsyncDelay(6000);
+  canHack = true;
+}
+void HackTheChromeBookV1(){
+
+  Serial.println("New Window");
     bleKeyboard.press(KEY_LEFT_CTRL);
     bleKeyboard.print("n");
     delay(400);
@@ -50,11 +95,56 @@ void loop() {
     bleKeyboard.print("q");
     delay(500);
     bleKeyboard.releaseAll();
-  }else
-  {
-    Serial.println("Disconnected");
-  }
-
-  Serial.println("Waiting 5 seconds...");
-  delay(5000);
 }
+
+void HackTheChromeBookV2(){
+  if(canHack == true)
+  {
+    canHack = false;
+    Serial.println("New Window");
+    bleKeyboard.press(KEY_LEFT_CTRL);
+    bleKeyboard.print("n");
+    AsyncDelay(400);
+    if(delayDone[delayItteration-1] == true)
+    {bleKeyboard.print("t");
+    bleKeyboard.release(KEY_LEFT_CTRL);
+
+    AsyncDelay(1500);
+    if(delayDone[delayItteration-1] == true)
+    //bleKeyboard.press(KEY_VOLUME_UP);
+
+    {Serial.println("Sending hacked message");
+    bleKeyboard.print("Your Being Hacked!");
+
+    AsyncDelay(2000);
+    if(delayDone[delayItteration-1] == true)
+    {Serial.println("Sending Rick");
+    bleKeyboard.press(KEY_LEFT_CTRL);
+    bleKeyboard.print("t");
+    bleKeyboard.release(KEY_LEFT_CTRL);
+    AsyncDelay(500);
+    if(delayDone[delayItteration-1] == true)
+    {bleKeyboard.print("https://www.youtube.com/watch?v=oPLObjVAvIU");
+    AsyncDelay(500);
+    if(delayDone[delayItteration-1] == true)
+    {Serial.println("Sending Enter key...");
+    bleKeyboard.write(KEY_RETURN);
+
+    AsyncDelay(7000);
+    if(delayDone[delayItteration-1] == true)
+    {Serial.println("Sending Shut Off");
+    bleKeyboard.press(KEY_LEFT_CTRL);
+
+    bleKeyboard.press(KEY_LEFT_SHIFT);
+    bleKeyboard.print("q");
+    AsyncDelay(100);
+    if(delayDone[delayItteration-1] == true)
+    {bleKeyboard.print("q");
+    AsyncDelay(500);
+    if(delayDone[delayItteration-1] == true)
+    {bleKeyboard.releaseAll();
+
+    HackDelay();
+    }}}}}}}}}
+}
+
